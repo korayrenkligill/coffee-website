ee9174884151dc62fcb91100bc3130270310066b
BuildTools <unconfigured@null.spigotmc.org>
2023-09-05 13:01:54 +0300

    some CEO settings have been made

--- src/components/pages/product-detail/product-detail.jsx
+++ src/components/pages/product-detail/product-detail.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ProductDetailHeader from "./sections/header/product-detail-header";
 import ProductDetailContent from "./sections/content/product-detail-content";
+import { Helmet } from "react-helmet";
 
 function ProductDetail() {
   const { no } = useParams();
@@ -12,6 +13,17 @@ function ProductDetail() {
   const product = productsJson.products.find((u) => u.no === no);
   return (
     <div className="ProductDetail">
+      <Helmet>
+        <title>{product.name}</title>
+        <meta
+          name="keywords"
+          content={`coffeact,coffee,coffee shop,cafe,${
+            product.name
+          },${product.materials.map((material) => {
+            return material;
+          })}`}
+        ></meta>
+      </Helmet>
       <ProductDetailHeader name={product.name} />
       <ProductDetailContent product={product} />
     </div>
